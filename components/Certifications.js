5e1b3645cdde9610e77799cabd0dead685cd4669
PSatyadevAbhiram <abhi.pandravada@gmail.com>
2023-08-10 17:47:33 -0400

    Added more styles and structured it better

--- components/Certifications.js
+++ components/Certifications.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import globalStyles from "./Globals.module.css";
+import certStyles from "./Certifications.module.css";
 
 const Certifications = () => {
   const certifications = useSelector((state) => state.certifications);
@@ -23,20 +24,20 @@ const Certifications = () => {
 
   return (
     <div>
-      <h3 className={globalStyles.h3}>Certifications</h3>
-      <ul className={globalStyles.ul}>
+      <h3 className={certStyles.h3}>Certifications</h3>
+      <ul className={certStyles.ul}>
         {Object.entries(groupedCertifications).map(([certName, certifications]) => (
           <li
             key={certName}
             onMouseEnter={() => setCertName(certName)}
             onMouseLeave={() => setCertName(null)}
           >
-            <div className={globalStyles.card}>
+            <div className={`${globalStyles.card} ${certStyles.card}` }>
               <strong>{certName}</strong>
               {hoveredCertName === certName && (
                 <ul>
                   {certifications.map((certification, index) => (
-                    <li className={globalStyles.cert_details} key={index}>
+                    <li className={certStyles.cert_details} key={index}>
                       Provider: {certification.provider} <br />
                       Validation Number: {certification.validationNumber} <br />
                       Verification URL: {certification.certificateURL} <br />
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,18 +4,18 @@ import navStyles from "./Navigation.module.css";
 
 const Navigation = () => {
     return(
-        <nav>
+        <nav className={navStyles.nav}>
             <ul className={navStyles.ul}>
                 <li className={navStyles.li}>
                     <Link href="/">Home</Link>
                 </li>
-                <li>
+                <li className={navStyles.li}>
                     <Link href="/contact">Contact</Link>
                 </li>
-                <li>
+                <li className={navStyles.li}>
                     <Link href="/about">About</Link>
                 </li>
-                <li>
+                <li className={navStyles.li}>
                     <Link href="/certifications">Certifications</Link>
                 </li>
             </ul>
