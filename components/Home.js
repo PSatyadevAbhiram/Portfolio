a8a91a237fc30d2d712b27dc16705b60ff5bf9f4
PSatyadevAbhiram <abhi.pandravada@gmail.com>
2023-08-11 02:20:15 -0400

    Completed styles to home page

--- components/Home.js
+++ components/Home.js
@@ -1,25 +1,27 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
+import globalStyles from "./Globals.module.css";
+import homeStyles from "./Home.module.css";
 
 export default function HomePage() {
   const technicalSkills = useSelector(state => state.skills);
 
   return (
     <Fragment>
-      <h1>Hello, I'm Abhiram.</h1>
-      <h2>Full Stack developer | Cloud-native | MERN | JavaScript</h2>
-      <div>
+      <h1 className={`${homeStyles.card} ${globalStyles.card}`}>Hello, I'm Abhiram.</h1>
+      <h2 className={homeStyles.sectionTitle}>Full Stack developer | Cloud-native | MERN | JavaScript</h2>
+      <div className={homeStyles.description}>
         Welcome to my portfolio! I am a passionate full-stack developer with
         experience in building web applications from front-end to back-end. I
         enjoy solving complex problems.
       </div>
-      <h3>Technical Skills</h3>
-      <table>
+      <h3 className={homeStyles.sectionTitle}>Technical Skills</h3>
+      <table className={homeStyles.skillsTable}>
         <tbody>
           {technicalSkills.map((skill, index) => (
             <tr key={index}>
-              <th>{skill.category}</th>
-              <td>{skill.skills}</td>
+              <th className={homeStyles.tableRow}>{skill.category}</th>
+              <td className={homeStyles.tableRow}>{skill.skills}</td>
             </tr>
           ))}
         </tbody>
